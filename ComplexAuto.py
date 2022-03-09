851a889f3f83d00e6904e37e21232320c4da04b7
GerasiMandMumu <konst.02@yandex.ru>
2022-03-09 20:15:26 +0300

    Add methods

--- ComplexAuto.py
+++ ComplexAuto.py
@@ -1,5 +1,33 @@
 class ComplexMotor:
-    def __init__(self, mileage):
-        self.mileage = mileage
+    def __init__(self, driver, car, fuel_type):
+        self.driver = driver
+        self.car = car
+        self.fuel_type = fuel_type
 
-obj1 = ComplexMotor(100)
+    # Расчет путевого листа
+    def calculationWaybill(self):
+        pass
+    
+    # Расчет пробега автомобиля
+    def setVehicleMileage(self, mileage):
+        self.mileage = mileage
+        
+    # Расчет фактического расхода топлива 
+    def setActualFuel(self, actual_feel):
+        self.actual_feel = actual_feel
+        
+    # Расчет расхода топлива по норме
+    def setConsumptionFuelNorm(self, fuel_norm):
+        self.fuel_norm = fuel_norm
+        
+    # Расчет числа отработанных часов
+    def setHoursWorked(self, hours):
+        self.hours = hours
+        
+    # Расчет числа тонно-километров
+    def setTonKilometers(self, ton_km):
+        self.ton_km = ton_km
+
+
+
+obj1 = ComplexMotor('Водитель', 'Машина', 'Топливо')
